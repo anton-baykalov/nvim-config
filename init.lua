d4cb6d9061c4ea013e81028944acee91f398eec5
Anton Baykalov <190809307+anton-baykalov@users.noreply.github.com>
2026-05-21 21:23:15 +0100

    initial nvim config

---
+++ init.lua
@@ -0,0 +1,71 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+  -- Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
+
+  { 'nvim-mini/mini.nvim', version = '*' },
+
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+
+
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      highlight = { enable = true },
+      ensure_installed = { "markdown", "markdown_inline", "latex" },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup()
+    end,
+  },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      latex = { enabled = true },
+    },
+  },
+
+})
