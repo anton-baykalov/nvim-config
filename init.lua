a0c4f4f5fc73e036bee004cefce94156e12f2ada
anton-baykalov <190809307+anton-baykalov@users.noreply.github.com>
2026-06-10 14:05:20 +0100

    no jetbrains stuff needed anymore

--- init.lua
+++ init.lua
@@ -1,19 +1,12 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "https://github.com/folke/lazy.nvim.git", lazypath })
 end
-
 vim.opt.rtp:prepend(lazypath)
-
 
 require("lazy").setup({
   -- Colorscheme
@@ -21,25 +14,24 @@ require("lazy").setup({
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
-    end,
+    config = function() vim.cmd.colorscheme("tokyonight") end,
   },
 
-  { 'nvim-mini/mini.nvim', version = '*' },
-
-  { "nvim-tree/nvim-web-devicons", opts = {} },
-
-
-
+  -- File tree (no icons)
   {
     "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-tree").setup({})
+      require("nvim-tree").setup({
+        renderer = {
+          icons = {
+            show = { file = false, folder = false, folder_arrow = false, git = false },
+          },
+        },
+      })
     end,
   },
 
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -48,24 +40,26 @@ require("lazy").setup({
       ensure_installed = { "markdown", "markdown_inline", "latex" },
     },
   },
+
+  -- Status line (no icons)
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("lualine").setup()
+      require("lualine").setup({
+        options = {
+          icons_enabled = false,
+          section_separators = "",
+          component_separators = "|",
+        },
+      })
     end,
   },
 
+  -- Markdown rendering
   {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      latex = { enabled = true },
-    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = { latex = { enabled = true } },
   },
-
 })
