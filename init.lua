b04838ba38c935b886e338dca7ad278c29453e71
anton-baykalov <190809307+anton-baykalov@users.noreply.github.com>
2026-06-10 15:16:11 +0100

    iron with sage

--- init.lua
+++ init.lua
@@ -8,6 +8,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.keymap.set("n", "<C-Left>",  "<C-w><")
+vim.keymap.set("n", "<C-Right>", "<C-w>>")
+vim.keymap.set("n", "<C-Up>",    "<C-w>+")
+vim.keymap.set("n", "<C-Down>",  "<C-w>-")
+
 require("lazy").setup({
   -- Colorscheme
   {
@@ -61,5 +66,51 @@ require("lazy").setup({
     ft = { "markdown" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = { latex = { enabled = true } },
+  },
+  
+  -- IPython REPL + cell execution
+  {
+    "Vigemus/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+      local view = require("iron.view")
+      local common = require("iron.fts.common")
+
+      iron.setup({
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            python = {
+              command = { "sage" },
+              format = common.bracketed_paste_python,
+              block_dividers = { "# %%", "#%%" },
+            },
+  sage = {                                -- ← add this
+    command = { "sage" },
+    format = common.bracketed_paste_python,
+    block_dividers = { "# %%", "#%%" },
+  },
+          },
+          repl_open_cmd = view.split.vertical.rightbelow("40%"),
+        },
+        keymaps = {
+          send_line     = "<leader>sl",
+          send_file     = "<leader>sf",
+          visual_send   = "<leader>sv",
+          interrupt     = "<leader>si",
+          exit          = "<leader>sq",
+          clear         = "<leader>sc",
+        },
+      })
+
+      -- Send current # %% cell
+      vim.keymap.set("n", "<leader>se", function()
+        iron.send_code_block()
+      end, { desc = "Send cell to IPython" })
+
+      -- Jump between cells
+      vim.keymap.set("n", "]c", function() vim.fn.search("^# %%", "W")  end, { desc = "Next cell" })
+      vim.keymap.set("n", "[c", function() vim.fn.search("^# %%", "bW") end, { desc = "Prev cell" })
+    end,
   },
 })
